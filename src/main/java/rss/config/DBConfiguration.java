149f95fc87802b67c7431fd6ce8b9f84a1d80cd7
Deviatkin Kirill <deviatkin-kirill@ya.ru>
2020-03-17 16:20:27 +0500

    add spring and database

---
+++ src/main/java/rss/config/DBConfiguration.java
@@ -0,0 +1,62 @@
+package rss.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import rss.config.properties.DBConfig;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@EnableTransactionManagement
+public class DBConfiguration {
+    @Autowired
+    private DBConfig config;
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(config.getDriver());
+        dataSource.setUrl(config.getUrl());
+        dataSource.setUsername(config.getUsername());
+        dataSource.setPassword(config.getPassword());
+        return dataSource;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean sessionFactory(DataSource dataSource) {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource);
+        em.setPackagesToScan("email.model");
+
+        JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        em.setJpaVendorAdapter(vendorAdapter);
+
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.temp.use_jdbc_metadata_defaults", "false");
+        properties.setProperty("hibernate.hbm2ddl.auto", config.getHbm2ddlAuto());
+        properties.setProperty("hibernate.dialect", config.getDialect());
+        properties.setProperty("hibernate.show_sql", config.getShowSql());
+        em.setJpaProperties(properties);
+
+        return em;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+
+        transactionManager.setEntityManagerFactory(emf);
+
+        return transactionManager;
+    }
+}
---
+++ src/main/java/rss/config/properties/DBConfig.java
@@ -0,0 +1,99 @@
+package rss.config.properties;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DBConfig {
+    @Value("${db.url}")
+    private String url;
+
+    @Value("${db.username}")
+    private String username;
+
+    @Value("${db.password}")
+    private String password;
+
+    @Value("${db.driver}")
+    private String driver;
+
+    @Value("${hibernate.dialect}")
+    private String dialect;
+
+    @Value("${hibernate.show_sql:true}")
+    private String showSql;
+
+    @Value("${hibernate.hbm2ddl.auto:validate}")
+    private String hbm2ddlAuto;
+
+    @Value("${entitymanager.packagesToScan:rss.model}")
+    private String packagesToScan;
+
+    //region getters and setters
+
+    public String getDriver() {
+        return driver;
+    }
+
+    public void setDriver(String driver) {
+        this.driver = driver;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getDialect() {
+        return dialect;
+    }
+
+    public void setDialect(String dialect) {
+        this.dialect = dialect;
+    }
+
+    public String getShowSql() {
+        return showSql;
+    }
+
+    public void setShowSql(String showSql) {
+        this.showSql = showSql;
+    }
+
+    public String getHbm2ddlAuto() {
+        return hbm2ddlAuto;
+    }
+
+    public void setHbm2ddlAuto(String hbm2ddlAuto) {
+        this.hbm2ddlAuto = hbm2ddlAuto;
+    }
+
+    public String getPackagesToScan() {
+        return packagesToScan;
+    }
+
+    public void setPackagesToScan(String packagesToScan) {
+        this.packagesToScan = packagesToScan;
+    }
+
+    //endregion
+}
