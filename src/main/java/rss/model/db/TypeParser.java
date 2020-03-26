5e5c288accf547247774d34e2d86cfd16edd3289
Deviatkin Kirill <deviatkin-kirill@ya.ru>
2020-03-26 05:49:48 +0500

    remove xsoup

--- src/main/java/rss/model/db/TypeParser.java
+++ src/main/java/rss/model/db/TypeParser.java
@@ -2,16 +2,5 @@ package rss.model.db;
 
 public enum TypeParser {
     Jsoup,
-    Xsoup,
-    XPath;
-
-//    private final Class<? extends Template> templateClass;
-//
-//    TypeParser(Class<? extends Template> templateClass) {
-//        this.templateClass = templateClass;
-//    }
-//
-//    public Class<? extends Template> getTemplateClass() {
-//        return templateClass;
-//    }
+    XPath
 }
