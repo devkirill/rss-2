29e35925eb877ec89861a75a08fc32fbd42a728a
Deviatkin Kirill <deviatkin-kirill@ya.ru>
2020-03-27 07:21:46 +0500

    modify date parser

--- src/main/java/rss/utils/DateUtil.java
+++ src/main/java/rss/utils/DateUtil.java
@@ -1,11 +1,16 @@
 package rss.utils;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class DateUtil {
     private final static List<String> FORMATS = Arrays.asList(
@@ -13,6 +18,8 @@ public class DateUtil {
             "yyyy-MM-dd'T'HH:mm:ssZ",
             "dd.MM.yyyy HH:mm:ss Z"
     );
+    private final static Pattern today = Pattern.compile("^\\s*(?:today|сегодня)\\s*(?:in|в)?\\s*(?<h>\\d{2}):(?<m>\\d{2})\\s*$");
+    private final static Pattern yesterday = Pattern.compile("^\\s*(?:yesterday|вчера)\\s*(?:in|в)?\\s*(?<h>\\d{2}):(?<m>\\d{2})\\s*$");
 
     public static ZonedDateTime parse(String stringDate, String format) {
         try {
@@ -28,6 +35,21 @@ public class DateUtil {
             ZonedDateTime parsedDate = parse(stringDate, format);
             if (parsedDate != null)
                 return parsedDate;
+        }
+
+        Matcher matchToday = today.matcher(stringDate);
+        if (matchToday.find()) {
+            String time = matchToday.group("h") + ":" + matchToday.group("m");
+            LocalTime localTime = LocalTime.parse(time, DateTimeFormatter.ofPattern("HH:mm"));
+            ZoneId zone = ZoneId.systemDefault();
+            return ZonedDateTime.of(LocalDate.now(zone), localTime, zone);
+        }
+        Matcher matchYesterday = yesterday.matcher(stringDate);
+        if (matchYesterday.find()) {
+            String time = matchYesterday.group("h") + ":" + matchYesterday.group("m");
+            LocalTime localTime = LocalTime.parse(time, DateTimeFormatter.ofPattern("HH:mm"));
+            ZoneId zone = ZoneId.systemDefault();
+            return ZonedDateTime.of(LocalDate.now(zone).minusDays(1), localTime, zone);
         }
 
         return null;
--- src/test/java/rss/utils/DateUtilTest.java
+++ src/test/java/rss/utils/DateUtilTest.java
@@ -1,8 +1,12 @@
 package rss.utils;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.ZoneId;
 import java.time.ZonedDateTime;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -21,5 +25,30 @@ public class DateUtilTest {
         assertNotNull(firstParse, String.format("String '%s' not parsed", firstDate));
         assertNotNull(secondParse, String.format("String '%s' not parsed", secondDate));
         assertEquals(firstParse.toEpochSecond(), secondParse.toEpochSecond());
+    }
+
+    @ParameterizedTest
+    @CsvSource(delimiter = ';', value = {
+            "15:48;today in 15:48",
+            "13:31;сегодня в 13:31",
+            "15:48;yesterday in 15:48",
+            "13:31;вчера в 13:31",
+    })
+    public void parseWithoutDateTest(String expectedTime, String parsedString) {
+        LocalTime time = LocalTime.parse(expectedTime);
+        ZonedDateTime parsedDate = DateUtil.parse(parsedString);
+        assertEquals(time, parsedDate.toLocalTime());
+    }
+
+    @Test
+    public void parseTodayWithoutDateTest() {
+        ZonedDateTime dateTime = DateUtil.parse("today in 15:48");
+        assertEquals(LocalDate.now(ZoneId.systemDefault()), dateTime.toLocalDate());
+    }
+
+    @Test
+    public void parseYesterdayWithoutDateTest() {
+        ZonedDateTime dateTime = DateUtil.parse("yesterday in 15:48");
+        assertEquals(LocalDate.now(ZoneId.systemDefault()).minusDays(1), dateTime.toLocalDate());
     }
 }
