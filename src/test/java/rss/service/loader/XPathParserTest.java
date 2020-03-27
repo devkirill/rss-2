8afc1fc2b61104c25e1d836287beba497a63cbb9
Deviatkin Kirill <deviatkin-kirill@ya.ru>
2020-03-27 09:53:27 +0500

    deleted an extra test

--- src/test/java/rss/service/loader/XPathParserTest.java
+++ src/test/java/rss/service/loader/XPathParserTest.java
@@ -42,23 +42,4 @@ public class XPathParserTest {
         assertEquals(ZonedDateTime.of(2020, 3, 17, 23, 11, 43, 0, ZoneId.of("GMT")), feed.getPosts().get(0).getPubDate());
         assertEquals("Kolobok86", feed.getPosts().get(0).getAuthor());
     }
-
-    @Test
-    public void parseHtmlTest() {
-        InputStream stream = this.getClass().getClassLoader().getResourceAsStream("tproger.html");
-        Template template = new Template();
-        template.setRoot("article");
-        template.setTitle("div[@class=post-title]");
-        template.setDescription("div[@class=entry-content]//p");
-        template.setLink(".//a");
-
-        Feed feed = parser.parseFeed(stream, template);
-
-        assertEquals(32, feed.getPosts().size());
-        assertTrue(feed.getPosts().get(0).getTitle().contains("Chrome 80 stable"));
-        assertTrue(feed.getPosts().get(0).getDescription().contains("Начали изучать что это такое"));
-        assertEquals("Tue, 17 Mar 2020 23:11:43 GMT", feed.getPosts().get(0).getRawPubDate());
-        assertEquals(ZonedDateTime.of(2020, 3, 17, 23, 11, 43, 0, ZoneId.of("GMT")), feed.getPosts().get(0).getPubDate());
-        assertEquals("Kolobok86", feed.getPosts().get(0).getAuthor());
-    }
 }
